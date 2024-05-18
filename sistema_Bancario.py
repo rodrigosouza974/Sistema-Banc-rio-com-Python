5953e9280e3a8972f0895293b68357e278e64165
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-05-18 18:11:16 -0300

    funções saque, deposito e extrato completas

--- sistema_Bancario.py
+++ sistema_Bancario.py
@@ -1,8 +1,7 @@
-import locale
 saldo=2000
 saque=0
 deposito= 0
-opcao = -1
+opcao =""
 contador_saques = 0
 lista_Saque =[]
 limite_Saque =[]
@@ -35,9 +34,10 @@ while opcao != 0:
 
     if opcao == 1:
         if saldo > 0:
+            saque =int(input("Qual a quantia deseja sacar: "))
             if saque <= 500 : 
                 if contador_saques <= max_saques:
-                    saque =int(input("Qual a quantia deseja sacar: "))
+                     #saque =int(input("Qual a quantia deseja sacar: "))
                     saldo -= saque
                     contador_saques += 1
                     print(f"Saque de {saque} realizado com sucesso!")
@@ -46,7 +46,7 @@ while opcao != 0:
                 else:
                     print("Você atingiu o limite máximo de saques permitidos.Sua operação foi cancelada.")
             else:
-                    print("Limite de Saque Excedido. Sua operação foi cancelada")
+                print("Limite de Saque Excedido. Sua operação foi cancelada")
         else:
             print("Você não possui saldo suficiente")
 
@@ -58,16 +58,26 @@ while opcao != 0:
 
       
     elif opcao == 3:
-        # Função para formatar os valores em reais
+        # Função para formatar os valores em reais saque
         def formatar_para_real(valor):
             return f"R${valor:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.')
 
         # Converter e imprimir a lista de saques formatados em reais
         saques_formatados = [formatar_para_real(saque) for saque in lista_Saque]
 
-        # Exibir os saques formatados
-        for saque_formatado in saques_formatados:
-            print(saque_formatado)
+        # Exibir os saques formatados com descrição
+        print("Esses são os saques realizados:")
+        for i, saque_formatado in enumerate(saques_formatados, start=1):
+            print(f"{i}º saque: {saque_formatado}")
+
+        # Converter e imprimir a lista de depositos formatados em reais
+        depositos_formatados = [formatar_para_real(deposito) for deposito in lista_Deposito]
+
+        # Exibir os depósitos formatados com descrição
+        print("Esses são os depósitos realizados:")
+        for i, deposito_formatado in enumerate(depositos_formatados, start=1):
+            print(f"{i}º depósito: {deposito_formatado}")
+        print(f"Seu saldo atual é de {saldo}")
 
     elif opcao == 0:
         print('Operação finalizada.')
